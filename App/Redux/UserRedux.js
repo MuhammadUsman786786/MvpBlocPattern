7c3399ec4e0a6271e56c4af709bfebcb9c708ea8
MuhammadUsman <m.usmanqau786@gmail.com>
2021-08-24 23:31:42 +0500

    error message added

--- App/Redux/UserRedux.js
+++ App/Redux/UserRedux.js
@@ -5,6 +5,7 @@ export const FETCH_USERS = createRoutine('FETCH_USERS');
 export const INITIAL_STATE = {
   users: [],
   fetchUsersLoading: false,
+  fetchUsersError: '',
 };
 
 export const reducer = (state = INITIAL_STATE, {type, payload}) => {
@@ -12,10 +13,12 @@ export const reducer = (state = INITIAL_STATE, {type, payload}) => {
     case FETCH_USERS.TRIGGER:
       return {...state, fetchUsersLoading: true};
     case FETCH_USERS.SUCCESS: {
-      return {...state,users:payload};
+      return {...state, users: payload};
     }
-    case FETCH_USERS.FAILURE:
-      return {...state};
+    case FETCH_USERS.FAILURE: {
+      const {message} = payload || {};
+      return {...state, fetchUsersError: message};
+    }
     case FETCH_USERS.FULFILL:
       return {...state, fetchUsersLoading: false};
 
