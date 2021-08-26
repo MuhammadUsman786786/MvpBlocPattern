2438282d6f3e5405dc90fd2b5800a621d46b9f32
MuhammadUsman <m.usmanqau786@gmail.com>
2021-08-26 17:24:19 +0500

    hot fixes

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -7,6 +7,7 @@ import {
   Text,
   View,
 } from 'react-native';
+
 import {useDispatch, useSelector} from 'react-redux';
 import {FETCH_USERS} from '../Redux/UserRedux';
 import {usersInfoSelector} from '../Redux/Selectors';
